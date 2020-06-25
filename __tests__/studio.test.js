8e384f4af87575df83c2d667dba0eeed472701ca
Logan Scott <logantscott@gmail.com>
2020-06-25 15:09:09 -0600

    studio get routes added, failing pending films model

--- __tests__/studio.test.js
+++ __tests__/studio.test.js
@@ -1,4 +1,5 @@
 require('../db/data-helpers');
+const { prepare } = require('../db/data-helpers');
 const Studio = require('../lib/models/Studio');
 const request = require('supertest');
 const app = require('../lib/app');
@@ -26,6 +27,23 @@ describe('my studio routes', () => {
           country: 'US'
         },
         __v: 0
+      }));
+  });
+  it('gets all studios', async() => {
+    const studios = prepare(await Studio.find().select({ name: true }));
+
+    return request(app)
+      .get('/api/v1/studios')
+      .then(res => expect(res.body).toEqual(studios));
+  });
+  it('gets a studio by id', async() => {
+    const studio = prepare(await Studio.findOne().select({ name: true, address: true }));
+
+    return request(app)
+      .get(`/api/v1/studios/${studio._id}`)
+      .then(res => expect(res.body).toEqual({
+        ...studio,
+        films: []
       }));
   });
 });
--- db/data-helpers.js
+++ db/data-helpers.js
@@ -6,9 +6,6 @@ const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
 const seed = require('./seed');
 
-const request = require('supertest');
-const app = require('../lib/app');
-
 beforeAll(async() => {
   const uri = await mongod.getUri();
   return connect(uri);
@@ -22,16 +19,19 @@ beforeEach(() => {
   return seed();
 });
 
-let agent = request.agent(app);
-beforeEach(async() => {
-  // agent
-});
-
 afterAll(async() => {
   await mongoose.connection.close();
   return mongod.stop();
 });
 
+const prepareOne = model => JSON.parse(JSON.stringify(model));
+const prepareMany = models => models.map(prepareOne);
+
+const prepare = model => {
+  if(Array.isArray(model)) return prepareMany(model);
+  return prepareOne(model);
+};
+
 module.exports = {
-  agent
+  prepare
 };
