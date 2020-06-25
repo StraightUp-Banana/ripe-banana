8318fced1e4bfb543e77a37b71e612e9bff6e0f7
Logan Scott <logantscott@gmail.com>
2020-06-25 14:22:30 -0600

    baseline

---
+++ data-helpers/data-helpers.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+const seed = require('./seed');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+beforeAll(async() => {
+  const uri = await mongod.getUri();
+  return connect(uri);
+});
+
+beforeEach(() => {
+  return mongoose.connection.dropDatabase();
+});
+
+beforeEach(() => {
+  return seed;
+});
+
+let agent = request.agent(app);
+beforeEach(async() => {
+  // agent
+});
+
+afterAll(async() => {
+  await mongoose.connection.close();
+  return mongod.stop();
+});
+
+module.exports = {
+  agent
+};
